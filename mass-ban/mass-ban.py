4ade394d0f88f610e63f3cbce652c7a5cfec35be
Kita <yunow6@gmail.com>
2022-02-10 19:11:23 +0400

    Update mass-ban.py

--- mass-ban/mass-ban.py
+++ mass-ban/mass-ban.py
@@ -16,7 +16,11 @@ class MassBan(commands.Cog):
     @commands.command(aliases=["mban"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def massban(self, ctx, members: commands.Greedy[discord.Member], days: typing.Optional[int] = 0, *, reason: str = None):
-        """*Mass-bans members*\n\nNotes:\nSeperate members by space.\nDeleted message for number of days is optional."""
+        """*Mass-bans members*\n
+        Notes
+        ├─ members - Seperate by space.
+        ├─ days - Deleted messages for number of days (optional; default is 0).
+        └─ reason - Self explanatory; optional."""
 
         if members is None:
             for member in members:
@@ -27,6 +31,8 @@ class MassBan(commands.Cog):
                 except Exception as e:
                     await ctx.send("An unexpected error occurred, please check the logs for more details.")
                     return
+        else:
+            ctx.send_help(ctx.command)
                         
 def setup(bot):
     bot.add_cog(MassBan(bot))
